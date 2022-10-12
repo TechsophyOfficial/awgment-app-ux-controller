1b6d15c7bef7cec82cd45b07e9b54fe6763645fd
Rajeev <rajeevkumar.s@techsophy.com>
2022-10-12 12:37:33 +0530

    Unit Testcase Code Coverage 22%

---
+++ src/test/java/com/techsophy/tsf/wrapperservice/config/JWTRoleConverterTest.java
@@ -0,0 +1,71 @@
+package com.techsophy.tsf.wrapperservice.config;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.techsophy.tsf.wrapperservice.utils.WebClientWrapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.nio.file.AccessDeniedException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class JWTRoleConverterTest {
+    @Mock
+    WebClientWrapper webClientWrapper;
+    @Mock
+    ObjectMapper objectMapper;
+
+    @InjectMocks
+    JWTRoleConverter jwtRoleConverter;
+
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(jwtRoleConverter, "keyCloakApi", "http://apigateway.techsophy.com");
+    }
+
+    @Test
+    void convertTest() throws JsonProcessingException {
+        Jwt jwt = Mockito.mock(Jwt.class);
+        List<String> awgmentRolesList = new ArrayList<>();
+        WebClient client = Mockito.mock(WebClient.class);
+        String userResponce = "userResponse";
+        Mockito.when(webClientWrapper.webclientRequest(any(), any(), any(), any())).thenReturn(userResponce);
+        Mockito.when(objectMapper.readValue(userResponce, Map.class)).thenReturn(Map.of("key", "val"));
+
+        Collection<GrantedAuthority> actualOutput = jwtRoleConverter.convert(jwt);
+        Collection<GrantedAuthority> expectedOutput = (awgmentRolesList).stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+        Assertions.assertSame(expectedOutput.size(), actualOutput.size());
+    }
+
+    @Test
+    void convertTestWhileThrowingException() throws JsonProcessingException {
+        Jwt jwt = Mockito.mock(Jwt.class);
+        List<String> awgmentRolesList = new ArrayList<>();
+        WebClient client = Mockito.mock(WebClient.class);
+        String userResponce = "";
+        Mockito.when(webClientWrapper.webclientRequest(any(), any(), any(), any())).thenReturn(userResponce);
+
+        Assertions.assertThrows(AccessDeniedException.class, () -> jwtRoleConverter.convert(jwt));
+    }
+}
---
+++ src/test/java/com/techsophy/tsf/wrapperservice/config/LocaleConfigTest.java
@@ -0,0 +1,57 @@
+package com.techsophy.tsf.wrapperservice.config;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+import static com.techsophy.tsf.wrapperservice.constants.ApplicationConstants.*;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+class LocaleConfigTest {
+
+    @Mock
+    HttpServletRequest request;
+    @InjectMocks
+    LocaleConfig localeConfig;
+
+    @Test
+    void resolveLocaleTest() {
+        Mockito.when(request.getHeader(any())).thenReturn("test");
+        Locale actualOutput = localeConfig.resolveLocale(request);
+        Locale expectedOutput = new Locale(request.getHeader(ACCEPT_LANGUAGE));
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void resolveLocaleEmptyHeaderTest() {
+        Mockito.when(request.getHeader(any())).thenReturn("");
+        Locale actualOutput = localeConfig.resolveLocale(request);
+        Locale expectedOutput = Locale.US;
+        Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void messageSourceTest() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasenames(BASENAME_ERROR_MESSAGES, BASENAME_MESSAGES);
+        messageSource.setCacheMillis(CACHEMILLIS);
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        messageSource.setUseCodeAsDefaultMessage(USEDEFAULTCODEMESSAGE);
+        MessageSource messageSource1 = messageSource;
+
+        MessageSource response = localeConfig.messageSource();
+        Assertions.assertEquals(response.getClass(), messageSource1.getClass());
+    }
+}
